7c4c1ad566083b4327544d3b5eef961d8af4e4d4
Adam Grare <adam@grare.com>
2016-06-29 16:49:40 -0400

    Need variadic macros for log.h

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,6 @@ autoreconf --force --install --symlink -I m4
 echo
 echo "--------------------------------------------------------------------------"
 echo "Initialized build system. For a common configuration please run:"
-echo "./configure CFLAGS='-g -ggdb -O0 -Wall -Wextra -Werror -pedantic -std=c89'"
+echo "./configure CFLAGS='-g -ggdb -O0 -Wall -Wextra -Werror'"
 echo "--------------------------------------------------------------------------"
 echo
